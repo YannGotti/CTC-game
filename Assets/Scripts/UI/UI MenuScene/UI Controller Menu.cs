a6cdcd54295ee27cd42474bcf6bcb58eec4e32cb
k1rson <vlad2k15@bk.ru>
2022-10-11 21:28:09 +0300

    fix 2

--- Assets/Scripts/UI/UI MenuScene/UI Controller Menu.cs
+++ Assets/Scripts/UI/UI MenuScene/UI Controller Menu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIController : MonoBehaviour
+public class UIControllerMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
