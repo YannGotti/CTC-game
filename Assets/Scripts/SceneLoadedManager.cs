bf3a028da16caa80e2b7958064ff5df0475c28d4
k1rson <vlad2k15@bk.ru>
2022-10-15 00:54:00 +0300

    add плавный (почти) переход между сценами.

---
+++ Assets/Scripts/SceneLoadedManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoadedManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _loadedPanel;  
+    [SerializeField] private Slider _progressBar;  
+    [SerializeField] private TMPro.TextMeshProUGUI _progressBarValue;
+
+    [SerializeField] private int _loadScene;
+
+    private void Awake()
+    {
+        _loadedPanel.SetActive(false); 
+    }
+
+    public void LoadScene()
+    {
+        _loadedPanel.SetActive(true);
+
+        StartCoroutine(LoadSceneAsync(_loadScene));
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit(); 
+    }
+
+    private IEnumerator LoadSceneAsync(int countScene)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(countScene);
+        
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress);
+
+            _progressBar.value = progress;
+            _progressBarValue.text = (progress * 100f).ToString("F0") + "%"; 
+
+            yield return null; 
+        }
+    }
+}
