2077a381d0ca934a45b993b19eb5050abdb50b68
YannGotti <1111111111aleksey46@gmail.com>
2022-10-18 00:47:34 +0300

    fixing database connect

--- Assets/Scripts/MySql/MySqlConnector.cs
+++ Assets/Scripts/MySql/MySqlConnector.cs
@@ -120,7 +120,7 @@ public class MySqlConnector : MonoBehaviour
 
         if (currentSteps == 0 || currentTime == 0)
         {
-            sql = $"INSERT INTO `users` (`count_step`, `time`) VALUES ('{steps}', '{time}') WHERE mac_address ='{macAdress}';";
+            sql = $"UPDATE `users` SET `count_step`= {steps}, `time`= {time} WHERE mac_address ='{macAdress}'";
         }
 
         if (steps < currentSteps && time < currentTime)
