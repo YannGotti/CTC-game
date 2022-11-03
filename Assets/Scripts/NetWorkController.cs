8eda0b3b52cacec3898588099319494db860cd14
YannGotti <1111111111aleksey46@gmail.com>
2022-11-03 19:26:45 +0300

    add animation news panels

--- Assets/Scripts/NetWorkController.cs
+++ Assets/Scripts/NetWorkController.cs
@@ -1,21 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NetWorkController : MonoBehaviour
 {
-    public void OpenTelegram()
-    {
-        Application.OpenURL("https://t.me/+PHiLMS7X_bI1Y2Ey");
-    }
+    public void OpenTelegram() => Application.OpenURL("https://t.me/+PHiLMS7X_bI1Y2Ey");
 
-    public void OpenDiscord()
-    {
-        Application.OpenURL("https://discord.gg/hRUygzgWx6");
-    }
+    public void OpenDiscord() => Application.OpenURL("https://discord.gg/hRUygzgWx6");
 
-    public void OpenGitHub()
-    {
-        Application.OpenURL("https://github.com/YannGotti/CTC-game");
-    }
+    public void OpenGitHub() => Application.OpenURL("https://github.com/YannGotti/CTC-game");
 }
---
+++ Assets/Scripts/UI/UI MainScene/UINewsPanelController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using UnityEngine;
+
+public class UINewsPanelController : MonoBehaviour
+{
+    [SerializeField] private float _speedAnimation;
+
+    private Transform _gameObject;
+
+
+    private void Start()
+    {
+        _speedAnimation = 10f;
+        _gameObject = gameObject.transform;
+    }
+
+    private void OnMouseEnter()
+    {
+        StopAllCoroutines();
+        StartCoroutine(AnimationPanel());
+    }
+
+    private void OnMouseExit()
+    {
+        StopAllCoroutines();
+        StartCoroutine(ResetPanel());
+    }
+
+    private IEnumerator AnimationPanel()
+    {
+        float timer = 0;
+
+        while (timer < _speedAnimation)
+        {
+
+            _gameObject.localEulerAngles = Vector3.Lerp(_gameObject.localEulerAngles, new Vector3(5, 5), _speedAnimation * Time.deltaTime);
+
+            timer += Time.deltaTime;
+
+            yield return null;
+        }
+
+        yield break;
+    }
+
+    private IEnumerator ResetPanel()
+    {
+        float timer = 0;
+
+        while (timer < _speedAnimation)
+        {
+            _gameObject.localEulerAngles = Vector3.Lerp(_gameObject.localEulerAngles, Vector3.zero, _speedAnimation * Time.deltaTime);
+
+            timer += Time.deltaTime;
+
+            yield return null;
+        }
+
+        yield break;
+    }
+
+
+}
